7a3af5bd6a66d7e19435d0e44cc6419247aeaf5e
Miguel Matrakas <mdmatrakas@gmail.com>
2019-08-19 19:36:19 -0300

    Inclusão do primeiro projeto no repositório.

---
+++ Aula_01/src/br/edu/udc/PrimeiraClasse.java
@@ -0,0 +1,15 @@
+package br.edu.udc;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		int valor = 34;
+		String str = "Uma frase!";
+		
+		System.out.println("Primeiro programa Java!!!");
+		System.out.println(str);
+		System.out.printf("Mostrando um número: %d\n", valor);
+
+	}
+
+}
